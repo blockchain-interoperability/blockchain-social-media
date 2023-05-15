b8616c8ec698351411373b3ac7b0eb7057ab046b
inwonakng <inwon.kang04@gmail.com>
2023-05-14 21:03:24 -0400

    deleting old files

--- twitter/draw_grams.sh
+++
@@ -1,7 +0,0 @@
-# bash
-
-source /data/blockchain-interoperability/blockchain-social-media/blockchain-sns-env/bin/activate
-cd /data/blockchain-interoperability/blockchain-social-media/analysis/twitter
-python3 main.py draw_ngrams_mixed
-python3 main.py draw_ngrams_emoji
-python3 main.py draw_ngrams_text
--- twitter/run_collect.sh
+++
@@ -1,7 +0,0 @@
-# bash
-
-source /data/blockchain-interoperability/blockchain-social-media/blockchain-sns-env/bin/activate
-cd /data/blockchain-interoperability/blockchain-social-media/analysis/twitter
-python3 main.py cache_index
-python3 main.py tokenize_text
-python3 main.py get_sbert_embeddings
