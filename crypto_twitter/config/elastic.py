2f9ea723d5ba719c93ac633482390bc23edc840b
inwonakng <inwon.kang04@gmail.com>
2023-09-27 04:36:50 -0400

    renamed config path

--- crypto_twitter/config/elastic.py
+++ crypto_twitter/config/elastic.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 KEYWORDS = yaml.safe_load(open(Path(__file__).parent/'yaml/keywords.yaml'))
 COLUMNS = yaml.safe_load(open(Path(__file__).parent/'yaml/columns.yaml'))
-ELASTIC_CONFIG = yaml.safe_load(open(Path(__file__).parent/'yaml/elastic_config.yaml'))
+ELASTIC_CONFIG = yaml.safe_load(open(Path(__file__).parent/'yaml/elastic.yaml'))
 
